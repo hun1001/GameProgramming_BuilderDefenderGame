b70195e8569ea63899992531d4627e7b1aa5626f
hun1001 <l01022899266@gmail.com>
2023-03-13 16:04:20 +0900

    버튼 선택 표시 ㅊ추가

--- Assets/Scripts/Building/BuildingManager.cs
+++ Assets/Scripts/Building/BuildingManager.cs
@@ -1,20 +1,24 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
 public class BuildingManager : MonoBehaviour
 {
-    //[SerializeField] private Transform pfWoodHarvester;
-    //[SerializeField] private BuildingTypeSO buildingType;
-    private BuildingTypeSO buildingType;
+    private static BuildingManager instance = null;
+    public static BuildingManager Instance => instance;
+
+    private BuildingTypeSO activeBuildingType;
+    public BuildingTypeSO ActiveBuildingType => activeBuildingType;
+
     private BuildingTypeListSO buildingTypeList;
 
     private Camera mainCamera;
 
     private void Awake()
     {
+        instance = this;
+
         buildingTypeList = Resources.Load<BuildingTypeListSO>(typeof(BuildingTypeListSO).Name);
-        buildingType = buildingTypeList.list[0];
+        activeBuildingType = buildingTypeList.list[0];
     }
     private void Start()
     {
@@ -23,22 +27,22 @@ public class BuildingManager : MonoBehaviour
 
     private void Update()
     {
-        if (Input.GetMouseButtonDown(0))
+        if (Input.GetMouseButtonDown(0) && !EventSystem.current.IsPointerOverGameObject())
         {
-            Instantiate(buildingType.prefab, GetMouseWorldPosition(), Quaternion.identity);
+            Instantiate(activeBuildingType.prefab, GetMouseWorldPosition(), Quaternion.identity);
         }
 
-        if (Input.GetKeyDown(KeyCode.Alpha0))
-        {
-            buildingType = buildingTypeList.list[0];
-        }
         if (Input.GetKeyDown(KeyCode.Alpha1))
         {
-            buildingType = buildingTypeList.list[1];
+            activeBuildingType = buildingTypeList.list[0];
         }
         if (Input.GetKeyDown(KeyCode.Alpha2))
         {
-            buildingType = buildingTypeList.list[2];
+            activeBuildingType = buildingTypeList.list[1];
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha3))
+        {
+            activeBuildingType = buildingTypeList.list[2];
         }
     }
 
@@ -48,4 +52,6 @@ public class BuildingManager : MonoBehaviour
         mouseWorldPosition.z = 0f;
         return mouseWorldPosition;
     }
+
+    public void SetActiveBuildingType(BuildingTypeSO buildingType) => activeBuildingType = buildingType;
 }
--- Assets/Scripts/Building/BuildingTypeSelectUI.cs
+++ Assets/Scripts/Building/BuildingTypeSelectUI.cs
@@ -1,10 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine.UI;
 using UnityEngine;
 
 public class BuildingTypeSelectUI : MonoBehaviour
 {
+    private Dictionary<BuildingTypeSO, Image> _buttonSelectDictionary;
+
     private void Awake()
     {
+        _buttonSelectDictionary = new Dictionary<BuildingTypeSO, Image>();
+
         var buildingTypeList = Resources.Load<BuildingTypeListSO>(typeof(BuildingTypeListSO).Name);
 
         Transform buildingTemplate = transform.Find("ButtonTemplate");
@@ -12,15 +18,33 @@ public class BuildingTypeSelectUI : MonoBehaviour
 
         int index = 0;
         const float defaultOffsetAmount = 20f;
-        const float offsetAmount = 100f;
+        const float offsetAmount = 140f;
 
         foreach (BuildingTypeSO buildingType in buildingTypeList.list)
         {
-            Transform resourceTransform = Instantiate(buildingTemplate, transform);
-            resourceTransform.gameObject.SetActive(true);
+            Transform buttonTransform = Instantiate(buildingTemplate, transform);
+            buttonTransform.gameObject.SetActive(true);
 
-            resourceTransform.GetComponent<RectTransform>().anchoredPosition = new Vector2(defaultOffsetAmount + (offsetAmount * index++), 10);
-            resourceTransform.GetChild(1).GetComponent<Image>().sprite = buildingType.iconSprite;
+            buttonTransform.GetComponent<RectTransform>().anchoredPosition = new Vector2(defaultOffsetAmount + (offsetAmount * index++), 10);
+            buttonTransform.GetChild(1).GetComponent<Image>().sprite = buildingType.iconSprite;
+
+            buttonTransform.GetComponent<Button>().onClick.AddListener(() =>
+            {
+                BuildingManager.Instance.SetActiveBuildingType(buildingType);
+                UpdateActiveBuildingTypeButton();
+            });
+
+            _buttonSelectDictionary[buildingType] = buttonTransform.GetChild(2).GetComponent<Image>();
+        }
+
+        Invoke(nameof(UpdateActiveBuildingTypeButton), Time.deltaTime);
+    }
+
+    private void UpdateActiveBuildingTypeButton()
+    {
+        foreach (BuildingTypeSO buildingType in _buttonSelectDictionary.Keys)
+        {
+            _buttonSelectDictionary[buildingType].enabled = (buildingType == BuildingManager.Instance.ActiveBuildingType);
         }
     }
 }
